9d49828108f0f76b6b95d02a63ee9e362a817a2a
SURAJ <mynisks@gmail.com>
2018-03-21 11:01:13 +0530

    lecture 37 added

---
+++ html/lecture37/hoverEventHandler.js
@@ -0,0 +1,14 @@
+$("#videos_menu").hover(function()
+{
+    $("#menu_feedback").html("Free videos tutorial for jquery");
+});
+
+$("#code_menu").hover(function()
+{
+    $("#menu_feedback").html("Free jquery codes");
+});
+
+$("#forum_menu").hover(function()
+{
+    $("#menu_feedback").html("Forum to discusse your jquery problems");
+});

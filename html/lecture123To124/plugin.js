14f9766537e693546a3020d3d36c56b4e1c85daf
SURAJ <mynisks@gmail.com>
2018-03-23 14:36:26 +0530

    passing option to plugin

---
+++ html/lecture123To124/plugin.js
@@ -0,0 +1,14 @@
+(function($)
+{
+    $.fn.highlight = function(options)
+    {
+        var settings ={"color" : null };
+        
+        if(options)
+        {
+            $.extend(settings,options)
+        }
+
+        $(this).css("background-color",settings["color"]);
+    }
+})(jQuery);

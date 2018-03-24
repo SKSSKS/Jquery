2553e53c1ebc6571b7bb85c51c3827ecb9923fbb
SURAJ <mynisks@gmail.com>
2018-03-24 11:14:14 +0530

    resizable

---
+++ html/lecture150/resizable.js
@@ -0,0 +1,9 @@
+$("#resize").resizable(
+    {
+         animate : true,
+         ghost : true,
+         animateDuration:2000,
+         animateEasing:"linear",
+         aspectRatio:true,
+         
+    })
